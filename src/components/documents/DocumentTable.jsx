d5f79a88f5e08c5354e7df5ebc00e66bfd1824f2
Harshad <mohamad.harshad@functionary.co>
2023-08-22 09:58:35 +0530

    added pagination

--- src/components/documents/DocumentTable.jsx
+++ src/components/documents/DocumentTable.jsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource theme-ui */
 
 import React, { useMemo } from "react"
-import { useTable } from "react-table"
+import { useTable, usePagination } from "react-table"
 import MockData from "./Mock_Data.json"
 import { Columns } from "./Columns"
 
@@ -18,11 +18,19 @@ import { Columns } from "./Columns"
 const TableComponent = ({ data }) => {
     const columns = useMemo(() => Columns, [])
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    const { getTableProps, getTableBodyProps, headerGroups, page, prepareRow, pageOptions,setPageSize, 
+        state: { pageIndex,pageSize },
+        gotoPage,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage, } =
         useTable({
             columns,
             data,
-        })
+            initialState: { pageIndex: 0, pageSize: 8 }, // Initial page index
+        }, usePagination // Use pagination feature
+        )
 
     return (
         <div>
@@ -39,7 +47,7 @@ const TableComponent = ({ data }) => {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
+                    {page.map((row) => {
                         prepareRow(row)
                         return (
                             <tr {...row.getRowProps()} key={row.id} sx={{height: "70px"}}>
@@ -55,7 +63,26 @@ const TableComponent = ({ data }) => {
                     })}
                 </tbody>
             </table>
+      
+            <div sx={{ display: "flex", marginTop: "28px", }}>
+                <div sx={{ height: "29px", py: "8px", px: "10px", border: "1px solid #E4E9EF", borderRadius : "4px", display: "flex"}}>
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {"<<"}First
+                </button>
+                <button onClick={() => gotoPage(pageOptions.length - 1)} disabled={!canNextPage}>
+                  Last{">>"}
+                </button>
+                </div>
+                
+                <div sx={{ marginLeft: "auto" }}>
+                    Page{" "}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{" "}
+                </div>
+             </div>
         </div>
+ 
     )
 }
 export default TableComponent
